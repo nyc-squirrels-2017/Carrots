8177eeb584eca9cc22b2e610cd6ed4351da41af1
Kendy Ng <email": null>
2017-01-27 15:12:15 -0500

    add error display for user registration

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,18 @@
+get '/users' do
+  redirect '/'
+end
+
 get '/users/new' do
   erb :"users/new"
+end
+
+post '/users' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id]
+    redirect '/users'
+  else
+    @errors = user.errors.full_messages
+    erb :"users/new"
+  end
 end
