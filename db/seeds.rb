36915bb216966a5e89ca7586894698d7350d0c76
Kendy Ng <email": null>
2017-01-27 17:09:03 -0500

    update seed again

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Guess.delete_all
 user1 = User.create!({
   :username => "admin",
   :email => "admin@gmail.com",
-  :password_digest => "$2a$10$LUOuwlIStjj9EI.jVr4w1uR2HTqtnmYvvVspDGnDj./MA7Zca7mC."
+  :password => "carrots"
   })
 
 deck1 = Deck.create!({
