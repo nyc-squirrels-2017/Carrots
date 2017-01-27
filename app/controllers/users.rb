0f96cadaebc8f9bb06a8325c7e2e9bf979429390
Adedotun <onidotun99@gmail.com>
2017-01-27 16:06:03 -0500

    Uncommented some things

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -5,23 +5,23 @@ get '/users/:id' do
 erb :users
 end
 
-# get '/users' do
-#   redirect '/'
-# end
-#
-# # signup
-# get '/users/signup' do
-#   erb :"users/new"
-# end
-#
-# post '/users' do
-#   user = User.new(params[:user])
-#   if user.save
-#     session[:user_id]
-#     redirect '/users'
-#   elses
-#     @errors = user.errors.full_messages
-#     erb :"users/new"
-#   end
-#
-# end
+get '/users' do
+  redirect '/'
+end
+
+# signup
+get '/users/signup' do
+  erb :"users/new"
+end
+
+post '/users' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id]
+    redirect '/users'
+  elses
+    @errors = user.errors.full_messages
+    erb :"users/new"
+  end
+
+end
