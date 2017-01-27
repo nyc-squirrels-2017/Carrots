31b2acc8f68e4abec4ce40cdec50a90b1ef5aa52
Josica <josica.ganesamurthy@gmail.com>
2017-01-27 11:48:24 -0500

    Create seed file

---
+++ db/seeds.rb
@@ -0,0 +1,44 @@
+User.delete_all
+Card.delete_all
+Deck.delete_all
+Round.delete_all
+Guess.delete_all
+
+user1 = User.create!({
+  :username => "admin",
+  :email => "admin@gmail.com",
+  :password_digest => "carrot"
+  })
+
+deck1 = Deck.create!({
+  :name => "Carrots"
+  })
+
+round1 = Round.create!({
+  :user_id => user1.id,
+  :deck_id => deck1.id
+    })
+
+card1 = Card.create!({
+  :question => "Are carrots orange?",
+  :answer => "true",
+  :deck_id => deck1.id
+  })
+
+card2 = Card.create!({
+    :question => "Does Bugs Bunny hate carrots?",
+    :answer => "false",
+    :deck_id => deck1.id
+    })
+
+guess1 = Guess.create!({
+  :response => true,
+  :round_id => round1.id,
+  :card_id => card1.id
+  })
+
+guess2 = Guess.create!({
+    :response => true,
+    :round_id => round1.id,
+    :card_id => card2.id
+    })
