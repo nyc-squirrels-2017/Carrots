946439b66ef679767a25a9a594fcc7c63509be6a
Kendy Ng <email": null>
2017-01-27 15:55:45 -0500

    added session helper methods and session login and logout logic

---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,19 @@
+def add_session(user_id)
+  session[:user_id] = user_id
+end
+
+def remove_session
+  session[:user_id] = nil
+end
+
+def current_user
+  @current_user ||= User.find_by_id(session[:user_id])
+end
+
+def login?
+  !!current_user
+end
+
+def require_login
+  redirect "/sessions/login" if current_user
+end
