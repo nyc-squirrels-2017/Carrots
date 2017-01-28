1f16fd54539d17a7d5b24189b0af5cbe083bc34f
Kendy Ng <email": null>
2017-01-27 23:35:04 -0500

    revised css for cards

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -12,10 +12,11 @@ get '/decks/:d_id/rounds/:round_num/cards/:id' do
   erb :"/cards/show"
 end
 
-# post '/decks/:d_id/rounds/:round_num/cards' do
+post '/decks/:d_id/rounds/:round_num/cards' do
+  binding.pry
   # @round = Round.where("user_id && deck_id &&")
-
-
+  #
+  #
   # Round.guesses.create(
   #       response: Guess.to_bool(params[:guess]),
   #       card_id: session[:user][:card_no]
@@ -26,4 +27,4 @@ end
   #   @errors = ['Incorrect!']
   #
   # end
-# end
+end
